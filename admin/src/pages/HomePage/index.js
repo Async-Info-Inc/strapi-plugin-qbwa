7ba45603d531a13bc4886def2a1a6f565745a684
filipszu <filipszu@gmail.com>
2023-04-23 23:37:38 -0400

    test of modifying plugin home page

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -1,7 +1,9 @@
 import React, { memo } from "react";
 
 const HomePage = () => {
-    return <></>;
+    return <>
+        <h1>QBWA - This is going to be a slog to edit</h1>
+    </>;
 };
 
 export default memo(HomePage);
