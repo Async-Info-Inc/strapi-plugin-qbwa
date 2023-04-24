ad070ae0cc525a9918c126bcadc6548c5976e449
filipszu <filipszu@gmail.com>
2023-04-24 14:46:59 -0400

    removing semicolon

--- admin/src/qbwa/components/header/index.jsx
+++ admin/src/qbwa/components/header/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 const Header = () => {
-    return (<div><h1>QBWA - Quickly Build Web</h1></div>);
+    return (<div><h1>QBWA - Quickly Build Web Apps</h1></div>);
 };
 export default Header;
