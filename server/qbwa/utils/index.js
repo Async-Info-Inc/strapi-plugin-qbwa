367cd5dc8e127cbfb3167e7dc907eb2229d49f02
filipszu <filipszu@gmail.com>
2023-04-24 15:59:46 -0400

    trying to access from the server side.

--- server/qbwa/utils/index.js
+++ server/qbwa/utils/index.js
@@ -1,4 +1,5 @@
-import fs from 'fs';
+const fs = require("fs");
+
 export const setUpPublic = () => {
     const publicFolder = "/";
     const qbwaPublic = `${publicFolder}/qbwa`;
--- server/register.js
+++ server/register.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const { setUpPublic } = require('./qbwa/utils');
+
 module.exports = ({ strapi }) => {
   // registeration phase
+  setUpPublic();
 };
