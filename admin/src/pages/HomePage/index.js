bd2023a51a6c61781c5104d054d2114596e95e0a
filipszu <filipszu@gmail.com>
2023-04-23 23:47:05 -0400

    Adding Monaco

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -1,8 +1,10 @@
+import { Editor } from "@monaco-editor/react";
 import React, { memo } from "react";
 
 const HomePage = () => {
     return <>
         <h1>QBWA - This is going to be a slog to edit</h1>
+        <Editor height="90vh" defaultLanguage="javascript" defaultValue="// some comment" />;
     </>;
 };
 
