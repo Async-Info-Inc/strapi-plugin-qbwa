5d2db3148bb0820d10489ef758967d811d719659
filipszu <filipszu@gmail.com>
2023-04-24 21:09:50 -0400

    creating qbwa folder

--- server/qbwa/utils/index.js
+++ server/qbwa/utils/index.js
@@ -4,7 +4,7 @@ const setUpPublic = () => {
     const publicFolder = "./public";
     const qbwaPublic = `${publicFolder}/qbwa`;
     console.log(">> QBWA", fs.readdirSync(publicFolder));
-    if(fs.existsSync(qbwaPublic)){
+    if(!fs.existsSync(qbwaPublic)){
         fs.mkdirSync(qbwaPublic);
     }
 };
