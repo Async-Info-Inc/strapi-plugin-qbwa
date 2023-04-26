423d71994b1f96302e348447bbdb48ae26408042
filipszu <filipszu@gmail.com>
2023-04-25 21:51:02 -0400

    inspecting query

--- server/controllers/stylesheets.js
+++ server/controllers/stylesheets.js
@@ -4,7 +4,7 @@ const { readStyleSheet } = require("../qbwa/utils");
 
 module.exports = {
     getStyleSheets: async (ctx) => {
-        console.log(">> QBWA: getStyleSheets controller", ctx);
+        console.log(">> QBWA: getStyleSheets controller", ctx.request.params, ctx.request.query);
         try {
             ctx.body = readStyleSheet();
         } catch (err) {
