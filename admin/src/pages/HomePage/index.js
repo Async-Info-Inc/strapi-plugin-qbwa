ce6bdcb78cc99c728c01d7358360e14faf4c0daf
filipszu <filipszu@gmail.com>
2023-04-25 23:35:52 -0400

    adding some UI

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -2,13 +2,23 @@ import React, { memo } from "react";
 import Header from "../../components/QBWA/header";
 import Body from "../../components/QBWA/body";
 import Footer from "../../components/QBWA/footer";
+import { Box } from '@strapi/design-system/Box';
+import { Layout,BaseHeaderLayout } from '@strapi/design-system/Layout';
+import { Button } from '@strapi/design-system/Button';
+import Plus from '@strapi/icons/Plus';
 
 const HomePage = () => {
-    return <div>
+    return <Box>
+        <Layout>
+            <Box background="neutral100">
+                <BaseHeaderLayout primaryAction={<Button startIcon={<Plus />}>Add a StyleSheet</Button>}
+                    title="Add StyleSheet" subtitle={``} as="h2" />
+            </Box>
         <Header />
         <Body />
         <Footer />
-    </div>
+        </Layout>
+    </Box>
 };
 
 export default memo(HomePage);
