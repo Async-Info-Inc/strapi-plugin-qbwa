92164b144a9dbf747a58f20a4ae134da0e78e641
filipszu <filipszu@gmail.com>
2023-04-26 16:47:34 -0400

    fixing api base

--- qbwa/fe/css.js
+++ qbwa/fe/css.js
@@ -1,22 +1,24 @@
 import { axiosInstance } from "../../admin/src/utils";
 
+const api_base = '/qbwa/css';
+
 const taskRequests = {
     getAllFiles: async () => {
-        const data = await axiosInstance.get(`/qbwa/styleSheets?name=index`);
+        const data = await axiosInstance.get(`${api_base}?name=index`);
         return data;
     },
     getFile: async (name) => {
-        const data = await axiosInstance.get(`/qbwa/styleSheets?name=${name}`);
+        const data = await axiosInstance.get(`${api_base}?name=${name}`);
         return data;
     },
     writeFile: async (name, data) => {
-        return await axiosInstance.post(`/qbwa/styleSheets`, {
+        return await axiosInstance.post(`${api_base}`, {
             name: name,
             data: data,
         });
     },
     deleteFile: async (name) => {
-        return await axiosInstance.post(`/qbwa/styleSheets`, {
+        return await axiosInstance.post(`${api_base}`, {
             name: name,
             action: 'delete'
         });
