bdd5f308a0ad628013ecb73282f8d894a30ea967
filipszu <filipszu@gmail.com>
2023-04-26 12:45:34 -0400

    log clean up

--- server/register.js
+++ server/register.js
@@ -4,6 +4,6 @@ const setUpPublic = require('../qbwa/be/css').setUpPublic;
 
 module.exports = ({ strapi }) => {
   // registeration phase
-  console.log(">> QBWA Plugin", "Registering plugin...");
+  console.log(">> 🗑", " Registering plugin...");
   setUpPublic();
 };
