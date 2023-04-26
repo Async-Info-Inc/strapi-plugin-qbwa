829e3245577088ea4ebfd8db263fa8a4e4da8efb
filipszu <filipszu@gmail.com>
2023-04-25 22:43:52 -0400

    inspecting post request

--- server/controllers/stylesheets.js
+++ server/controllers/stylesheets.js
@@ -14,7 +14,8 @@ module.exports = {
         }
     },
     setStyleSheet: async (ctx) => {
-        console.log(">> QBWA: setStyleSheet controller", ctx.request.params, ctx.request.query);
+        const { body } = ctx.request;
+        console.log(">> QBWA: setStyleSheet controller", ctx.request);
         try {
             const name = ctx.request.query.name || 'index';
             ctx.body = createStyleSheet(name);
