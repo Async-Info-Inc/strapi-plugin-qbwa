a06758fb757beb3d96700f42799481ce7b27da4e
filipszu <filipszu@gmail.com>
2023-04-26 16:30:34 -0400

    adding the select

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -1,22 +1,33 @@
-import React, { memo } from "react";
-import Header from "../../components/QBWA/header";
-import Body from "../../components/QBWA/body";
-import Footer from "../../components/QBWA/footer";
+import React, { memo, useEffect, useState } from "react";
 import { Box } from '@strapi/design-system/Box';
 import { Layout,BaseHeaderLayout } from '@strapi/design-system/Layout';
 import { Button } from '@strapi/design-system/Button';
 import Plus from '@strapi/icons/Plus';
+import api from "../../../../qbwa/fe/css";
 
 const HomePage = () => {
+    const [fileOptions, setFileOptions] = useState([]);
+
+    useEffect(() => {
+        api.getAllFiles().then((data) => {
+            setFileOptions(data.data);
+        });
+    }, []);
+
+    const selectOptions = fileOptions.map((file) => {
+        return <SingleSelectOption value={file}>{file}</SingleSelectOption>
+    });
+
     return <Box>
         <Layout>
             <Box background="neutral100">
-                <BaseHeaderLayout primaryAction={<Button startIcon={<Plus />}>Add a StyleSheet</Button>}
-                    title="Add StyleSheet" subtitle={``} as="h2" />
+                <BaseHeaderLayout title="QBWA" subtitle={`Quickly Build Web Apps`} as="h2" />
             </Box>
-        <Header />
-        <Body />
-        <Footer />
+            <Box padding={8} background="neutral100">
+            <SingleSelect label="Fruits" required placeholder="Select file..." hint="The StyleSheet you want to edit.">
+                {selectOptions}
+            </SingleSelect>
+            </Box>
         </Layout>
     </Box>
 };
--- qbwa/fe/css.js
+++ qbwa/fe/css.js
@@ -1,14 +1,24 @@
 import { axiosInstance } from "../../admin/src/utils/axiosInstance";
 
 const taskRequests = {
-    getFile: async () => {
+    getAllFiles: async () => {
         const data = await axiosInstance.get(`/qbwa/styleSheets?name=index`);
         return data;
     },
-    createFile: async (name, data) => {
+    getFile: async (name) => {
+        const data = await axiosInstance.get(`/qbwa/styleSheets?name=${name}`);
+        return data;
+    },
+    writeFile: async (name, data) => {
         return await axiosInstance.post(`/qbwa/styleSheets`, {
             name: name,
             data: data,
+        });
+    },
+    deleteFile: async (name) => {
+        return await axiosInstance.post(`/qbwa/styleSheets`, {
+            name: name,
+            action: 'delete'
         });
     }
 };
