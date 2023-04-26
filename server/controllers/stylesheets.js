ec24e9ebb7aa3fa9ac46bb1a0efda408b7d098f6
filipszu <filipszu@gmail.com>
2023-04-25 21:56:08 -0400

    actualy reading the stylesheet by name

--- server/controllers/stylesheets.js
+++ server/controllers/stylesheets.js
@@ -6,7 +6,8 @@ module.exports = {
     getStyleSheets: async (ctx) => {
         console.log(">> QBWA: getStyleSheets controller", ctx.request.params, ctx.request.query);
         try {
-            ctx.body = readStyleSheet();
+            const name = ctx.request.query.name || 'index';
+            ctx.body = readStyleSheet(name);
         } catch (err) {
             ctx.body = err;
             ctx.throw(500, err);
