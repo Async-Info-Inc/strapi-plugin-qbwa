b22c92926d4e2b0d8cc66aae0cb0c88952a09c97
filipszu <filipszu@gmail.com>
2023-04-28 16:22:13 -0400

    removing the file

--- admin/src/components/QBWA/Body/index.jsx
+++ admin/src/components/QBWA/Body/index.jsx
@@ -8,14 +8,25 @@ import Trash from '@strapi/icons/Trash';
 
 const Body = () => {
     const [fileOptions, setFileOptions] = useState([]);
+    const [selectedFile, setSelectedFile] = useState(null);
     const [editorModel, setEditorModel] = useState("");
     const onChange = (value) => {
         console.log("On Change: ", value);
+        setSelectedFile(value);
+    };
+
+    const onRemoveClick = () => {
+        api.deleteFile(selectedFile);
+        setFileOptions(fileOptions => fileOptions.filter(file => file !== selectedFile));
+        onChange(fileOptions[0]);
+    };
+
+    useEffect(() => {
         api.getFile(value).then((data) => {
             console.log("Get File: ", data);
             setEditorModel(data);
         });
-    };
+    }, [selectedFile]);
 
     useEffect(() => {
         api.getAllFiles().then((data) => {
@@ -28,7 +39,7 @@ const Body = () => {
     return (
         <div>
             <SingleSelect label="CSS File" options={fileOptions} onChange={onChange} /> 
-            <Button variant="danger" startIcon={Trash}>Remove File</Button>
+            <Button variant="danger" startIcon={Trash} onClick={onRemoveClick}>Remove File</Button>
             <Editor height="90vh" defaultLanguage="css" defaultValue="" value={editorModel} />
         </div>
     );
