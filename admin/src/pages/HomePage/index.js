a62b8e24ca21adab68689d73c5020e24b08557d7
filipszu <filipszu@gmail.com>
2023-04-27 20:07:32 -0400

    adding fruit single select

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -1,11 +1,15 @@
 import React, { memo, useEffect, useState } from "react";
 import { Box } from '@strapi/design-system/Box';
-import { Layout, BaseHeaderLayout, Button } from '@strapi/design-system';
+import { Layout, BaseHeaderLayout, Button, SingleSelect, SingleSelectOption } from '@strapi/design-system';
 import Plus from '@strapi/icons/Plus';
 import api from "../../../../qbwa/fe/css";
 
 const HomePage = () => {
     const [fileOptions, setFileOptions] = useState([]);
+
+    const [value, setValue] = useState();
+    const [error, toggleError] = useState();
+    const [disabled, toggleDisabled] = useState();
 
     useEffect(() => {
         api.getAllFiles().then((data) => {
@@ -25,9 +29,17 @@ const HomePage = () => {
                 <BaseHeaderLayout title="QBWA" subtitle={`Quickly Build Web Apps`} as="h2" />
             </Box>
             <Box padding={8} background="neutral100">
-                {/* <SingleSelect label="StyleSheets" placeholder="Select file..." hint="The StyleSheet you want to edit."> */}
-                    {/* {selectOptions} */}
-                {/* </SingleSelect> */}
+                <SingleSelect label="Fruits" required placeholder="My favourite fruit is..." hint="Fruits are not vegetables" onClear={() => {
+                    setValue(undefined);
+                    }} error={error} value={value} onChange={setValue} disabled={disabled}>
+                            <SingleSelectOption value="apple">Apple</SingleSelectOption>
+                            <SingleSelectOption value="avocado">Avocado</SingleSelectOption>
+                            <SingleSelectOption value="banana">Banana</SingleSelectOption>
+                            <SingleSelectOption value="kiwi">Kiwi</SingleSelectOption>
+                            <SingleSelectOption value="mango">Mango</SingleSelectOption>
+                            <SingleSelectOption value="orange">Orange</SingleSelectOption>
+                            <SingleSelectOption value="strawberry">Strawberry</SingleSelectOption>
+                </SingleSelect>
                 <Button variant="success">Add</Button>
                 <Button startIcon={Plus} variant="danger">Remove</Button>
             </Box>
