690fa45831ffb9c663cc1b80979a5935f06c4dfe
filipszu <filipszu@gmail.com>
2023-04-27 20:44:03 -0400

    removing fruit selector

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -1,6 +1,6 @@
 import React, { memo, useEffect, useState } from "react";
 import { Box } from '@strapi/design-system/Box';
-import { Layout, BaseHeaderLayout, Button, SingleSelect, SingleSelectOption } from '@strapi/design-system';
+import { Layout, BaseHeaderLayout, Button } from '@strapi/design-system';
 import Plus from '@strapi/icons/Plus';
 import api from "../../../../qbwa/fe/css";
 
@@ -29,17 +29,6 @@ const HomePage = () => {
                 <BaseHeaderLayout title="QBWA" subtitle={`Quickly Build Web Apps`} as="h2" />
             </Box>
             <Box padding={8} background="neutral100">
-                <SingleSelect label="Fruits" required placeholder="My favourite fruit is..." hint="Fruits are not vegetables" onClear={() => {
-                    setValue(undefined);
-                    }} error={error} value={value} onChange={setValue} disabled={disabled}>
-                            <SingleSelectOption value="apple">Apple</SingleSelectOption>
-                            <SingleSelectOption value="avocado">Avocado</SingleSelectOption>
-                            <SingleSelectOption value="banana">Banana</SingleSelectOption>
-                            <SingleSelectOption value="kiwi">Kiwi</SingleSelectOption>
-                            <SingleSelectOption value="mango">Mango</SingleSelectOption>
-                            <SingleSelectOption value="orange">Orange</SingleSelectOption>
-                            <SingleSelectOption value="strawberry">Strawberry</SingleSelectOption>
-                </SingleSelect>
                 <Button variant="success">Add</Button>
                 <Button startIcon={Plus} variant="danger">Remove</Button>
             </Box>
