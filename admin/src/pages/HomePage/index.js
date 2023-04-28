f700c2db30d9e7106cfa475f13aa302e65162bc6
filipszu <filipszu@gmail.com>
2023-04-27 22:14:39 -0400

    added normal select

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -7,28 +7,27 @@ import api from "../../../../qbwa/fe/css";
 const HomePage = () => {
     const [fileOptions, setFileOptions] = useState([]);
 
-    const [value, setValue] = useState();
-    const [error, toggleError] = useState();
-    const [disabled, toggleDisabled] = useState();
-
     useEffect(() => {
         api.getAllFiles().then((data) => {
             console.log("Get All Files: ", data);
             setFileOptions(data.data);
         });
     }, []);
-/*
+
     const selectOptions = fileOptions.map((file) => {
         console.log("Creating file option: ", file);
-        return <SingleSelectOption value={file}>{file}</SingleSelectOption>
+        return <option value={file}>{file}</option>
     });
-*/
+
     return <Box>
         <Layout>
             <Box background="neutral100">
                 <BaseHeaderLayout title="QBWA" subtitle={`Quickly Build Web Apps`} as="h2" />
             </Box>
             <Box padding={8} background="neutral100">
+                <select>
+                    {selectOptions}
+                </select>
                 <Button variant="success">Add</Button>
                 <Button startIcon={Plus} variant="danger">Remove</Button>
             </Box>
