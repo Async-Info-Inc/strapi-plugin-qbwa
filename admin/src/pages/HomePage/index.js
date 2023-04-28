f9f97a81cd650b9730576b4241a236fbd0c190c0
filipszu <filipszu@gmail.com>
2023-04-28 14:00:21 -0400

    basic select

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -17,33 +17,17 @@ import Body from "../../components/QBWA/Body";
 import Footer from "../../components/QBWA/Footer";
 
 const HomePage = () => {
-    const [value, setValue] = useState();
-    const [error, toggleError] = useState();
-    const [disabled, toggleDisabled] = useState();
-
-
-
-    return <Flex direction="column" alignItems="center" gap={11}>
-          <SingleSelect label="Fruits" required placeholder="My favourite fruit is..." hint="Fruits are not vegetables" onClear={() => {
-      setValue(undefined);
-    }} error={error} value={value} onChange={setValue} disabled={disabled}>
-            <SingleSelectOption value="apple">Apple</SingleSelectOption>
-            <SingleSelectOption value="avocado">Avocado</SingleSelectOption>
-            <SingleSelectOption value="banana">Banana</SingleSelectOption>
-            <SingleSelectOption value="kiwi">Kiwi</SingleSelectOption>
-            <SingleSelectOption value="mango">Mango</SingleSelectOption>
-            <SingleSelectOption value="orange">Orange</SingleSelectOption>
-            <SingleSelectOption value="strawberry">Strawberry</SingleSelectOption>
-          </SingleSelect>
-          <Flex gap={2}>
-            <Button variant="danger-light" onClick={() => toggleError(s => s ? undefined : 'Oh no, the fruits have gone mouldy!')}>
-              {`${error ? 'Hide' : 'Show'} the error state`}
-            </Button>
-            <Button variant="tertiary" onClick={() => toggleDisabled(s => !s)}>
-              {`${disabled ? 'Hide' : 'Show'} the disabled state`}
-            </Button>
-          </Flex>
-        </Flex>;
+    return <Box>
+        <Layout>
+            <select>
+                <option>1</option>
+                <option>2</option>
+            </select>
+            {/* <Header />
+            <Body />
+            <Footer /> */}
+        </Layout>
+    </Box>
 };
 
 export default memo(HomePage);
