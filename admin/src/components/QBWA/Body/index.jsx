fcb5498983d784aec1a6967fd3819f5ce8f1eb30
filipszu <filipszu@gmail.com>
2023-04-28 16:13:05 -0400

    added remove button

--- admin/src/components/QBWA/Body/index.jsx
+++ admin/src/components/QBWA/Body/index.jsx
@@ -1,7 +1,9 @@
 import { Editor } from '@monaco-editor/react';
 import React, {useEffect, useState} from 'react';
+import {Button} from '@strapi/design-system';
 import SingleSelect from '../SingleSelect';
 import api from '../../../../../qbwa/fe/css';
+import Trash from '@strapi/icons/Trash';
 // import SingleSelect from '../SingleSelect';
 
 const Body = () => {
@@ -25,6 +27,7 @@ const Body = () => {
     return (
         <div>
             <SingleSelect label="CSS File" options={fileOptions} onChange={onChange} /> 
+            <Button variant="danger" startIcon={Trash}>Remove File</Button>
             <Editor height="90vh" defaultLanguage="css" defaultValue="" value={editorModel} />
         </div>
     );
