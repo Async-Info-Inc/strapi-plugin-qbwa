b07d8a39b1b7e6d95d181c462db6595a8d992b04
filipszu <filipszu@gmail.com>
2023-04-27 23:42:11 -0400

    adding logs

--- admin/src/components/QBWA/SingleSelect/index.jsx
+++ admin/src/components/QBWA/SingleSelect/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-const SingleSelect = ({ label = "label: ", options = [{label: 'option 1', value: 1}], value = 1, onChange }) => {
+const SingleSelect = ({ label = "label: ", options, value, onChange }) => {
     const handleChange = (e) => {
         console.log(`SingleSelect: handleChange: ${e.target.value}`);
         onChange(e.target.value);
@@ -9,11 +9,12 @@ const SingleSelect = ({ label = "label: ", options = [{label: 'option 1', value:
         <div className="form-group">
         <label>{label}</label>
         <select className="form-control" value={value} onChange={handleChange}>
-            {options.map((option) => (
-            <option key={option.value} value={option.value}>
-                {option.label}
-            </option>
-            ))}
+            {options.map((option) => {
+                console.log(`Making option for: ${JSON.stringify(option)}`);
+                return <option key={option.value} value={option.value}>
+                    {option.label}
+                </option>;
+            })}
         </select>
         </div>
     );
