73d1929246b69fddb865d382d66d641a8d17cf60
filipszu <filipszu@gmail.com>
2023-04-30 11:32:18 -0400

    setting selected value

--- admin/src/components/QBWA/SingleSelect/index.jsx
+++ admin/src/components/QBWA/SingleSelect/index.jsx
@@ -6,7 +6,7 @@ const SingleSelect = ({ label = "label: ", options = [], value, onChange }) => {
         `options: ${Logger.arrayToString(options)}`
     ]);
     const optionEls = options.map((option) => {
-        return <option value={option}>{option}</option>;
+        return <option value={option} selected={option === value ? true : false}>{option}</option>;
     });
     const onSelectChange = (event) => {
         const value = event.target.value;
@@ -20,7 +20,7 @@ const SingleSelect = ({ label = "label: ", options = [], value, onChange }) => {
     return (
         <div>
             <label>{label}</label>
-            <select onChange={onSelectChange} value={value}>
+            <select onChange={onSelectChange}>
                 {optionEls}
             </select>
         </div>
